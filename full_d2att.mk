a195a5c5e1189e829ce2e5bb5005c2074fd65974
Shareef Ali <shareefalis@gmail.com>
2012-08-26 22:23:52 -0500

    d2: move to d2-common
    Change-Id: I3c710d3ec7ee7d2d1be38fe5da24bcd0c089c880

--- full_d2att.mk
+++ full_d2att.mk
@@ -21,10 +21,6 @@
 # lines, full and maguro, hence its name.
 #
  
-# Torch
-PRODUCT_PACKAGES := \
-    Torch
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d2att device
