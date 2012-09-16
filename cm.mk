4de95dde4ebbc6f3f112ec8baf482f1b56a32d37
chaosagent <davidh@cpu2.co.cc>
2012-09-15 18:27:24 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -11,6 +11,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2uc-user 4.0.4 IMM76D I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALEM:user/release-keys"
 
-PRODUCT_NAME := full_d2att
+PRODUCT_NAME := cm_d2att
 PRODUCT_DEVICE := d2att
 
