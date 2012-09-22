0a56d82ca26941272df4cab75cad415076b10a30
nocoast <voteforpedro8@gmail.com>
2012-09-22 16:03:24 +0000

    Liquify d2att

--- liquid.mk
+++ liquid.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2uc-user 4.0.4 IMM76D I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALEM:user/release-keys"
 
-PRODUCT_NAME := cm_d2att
+PRODUCT_NAME := liquid_d2att
 PRODUCT_DEVICE := d2att
 
