00580c1428aaa213d36a05b1787b9456a6ac30e3
Eric Clothier <eric.clothier@gmail.com>
2014-11-12 20:16:30 -0500

    d2att: update props, blobs, asserts, and overlays
    BoardConfig.mk: Add d2lte to the TARGET_OTA_ASSERT_DEVICE to support
                    flashing to i747 variants currently running a d2lte
                    build
    
    cm.dependencies: Pulling the d2-common tree pulls all of the needed
                     trees
    
    cm.mk: Update build fingerprint
    s
    device.mk: Remove the d2-common/overlay-gsm, as it no longer exists
    
    proprietary-files.txt: add the libril.so and libsec-ril.so files for
                           the d2att specifically. This is needed since
                           the d2-common extract-files.sh no longer pulls
                           device specific files, like it used to with
                           d2-common/proprietary-files.txt (not to be
                           confused with the current
                           d2-common/proprietary-files.txt, which is
                           strictly common d2 files only) Also rename
                           from device-proprietary-files.txt, as the core
                           file extraction script, located in msm8960-common
                           does not find device-proprietary-files.txt
    
    extract-files.sh: Make a call directly to msm8960-common/extract-files.sh,
                      otherwise the files listed in proprietary-files.txt are
                      ignored
    
    Change-Id: I7653ed5eccf10b626469575c4f9bc3cae68b108b

--- device.mk
+++ device.mk
@@ -19,8 +19,6 @@ $(call inherit-product-if-exists, vendor/samsung/d2att/d2att-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2att/overlay
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ "$1" != "" ]; then
+    export SOURCEDIR=$1
+fi
+
 export DEVICE=d2att
 export VENDOR=samsung
 ./../d2-common/extract-files.sh $@
+./../msm8960-common/extract-files.sh $@
