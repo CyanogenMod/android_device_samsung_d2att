c478e063706a868b38e0593dfef20ee72f33d8cf
Eric Clothier <eric.clothier@gmail.com>
2014-11-15 19:33:35 -0500

    d2att: update props, blobs, asserts, and overlays
    BoardConfig.mk: Add d2lte to the TARGET_OTA_ASSERT_DEVICE to support
                    flashing to i747 variants currently running a d2lte
                    build
    
    cm.dependencies: Pulling the d2-common tree pulls all of the needed
                     trees
    
    cm.mk: Update build fingerprint
    s
    device.mk: Remove the d2-common/overlay-gsm, as it no longer exists
    
    proprietary-files.txt: add the libril.so and libsec-ril.so files for
                           the d2att specifically. This is needed since
                           the d2-common extract-files.sh no longer pulls
                           device specific files, like it used to with
                           d2-common/proprietary-files.txt (not to be
                           confused with the current
                           d2-common/proprietary-files.txt, which is
                           strictly common d2 files only) Also rename
                           from device-proprietary-files.txt, as the core
                           file extraction script, located in msm8960-common
                           does not find device-proprietary-files.txt
    
    extract-files.sh: Make a call directly to msm8960-common/extract-files.sh,
                      otherwise the files listed in proprietary-files.txt are
                      ignored
    
    Change-Id: I8ebc5ef95d52c369cff8b9d197f355de4c245afe

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/I747UCDMG2:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K I747UCDMG2 release-keys"
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUEMJB:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUEMJB release-keys"
 
 PRODUCT_NAME := cm_d2att
 PRODUCT_DEVICE := d2att
