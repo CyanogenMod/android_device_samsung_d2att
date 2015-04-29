4d5db725e124ce72b25de4d67b7fc35c1ca531a8
Dave Daynard <nardholio@gmail.com>
2015-04-28 20:57:34 -0400

    d2att: update build fingerprint
    Change-Id: Idcd96ab0692c6e06e9280f18dfdca15c9cfde487

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNE4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNE4 release-keys"
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ1:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ1 release-keys"
 
 PRODUCT_NAME := cm_d2att
 PRODUCT_DEVICE := d2att
