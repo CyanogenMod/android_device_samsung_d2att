1afc850911dc5927f5125a89710688a60fe26606
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    d2att: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: I1a23b074ed1071649afde0900556851174c8f4df

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
